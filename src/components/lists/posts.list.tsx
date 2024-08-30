a8240651b3f604c72766b3cfa14c72be881622f1
alex <arusescu@dev-id.fr>
2024-08-30 15:18:55 +0200

    fix delete post on profile

--- src/components/lists/posts.list.tsx
+++ src/components/lists/posts.list.tsx
@@ -1,20 +1,30 @@
 import PostCard from "@components/cards/post.card";
 import { PostDto } from "@stores/post/post.model";
+import { toast } from "sonner";
+import PostRequest from "../../stores/post/post.request";
 
 interface PostsListProps {
-    posts: PostDto[]
+  posts: PostDto[];
+  onDelete: (postId: string) => void;
 }
 
 const PostsList = (props: PostsListProps) => {
-    const { posts } = props;
+  const { posts, onDelete } = props;
 
-    return (
-        <div className="flex flex-col gap-2">
-            {posts.map((x) => (
-                <PostCard key={x.id} post={x} />
-            ))}
-        </div>
-    )
-}
+  const handleDeletePost = (postId: string) => {
+    PostRequest.deletePost(postId).then(() => {
+      onDelete(postId);
+      toast.success("Post supprimé avec succès");
+    });
+  };
 
-export default PostsList
+  return (
+    <div className="flex flex-col gap-2">
+      {posts.map((x) => (
+        <PostCard key={x.id} post={x} onDelete={handleDeletePost} />
+      ))}
+    </div>
+  );
+};
+
+export default PostsList;
